34d6c3a1a7751874a9452e6c17239f9d05158720
jveres <janos.veres@gmail.com>
2019-10-29 23:19:23 +0100

    Initial upload

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function (api) {
+    api.cache(true);
+  
+    const presets = [
+        [
+            "@babel/preset-env",
+            {
+            "targets": {
+                "node": "10"
+            }
+            }
+        ]
+    ];
+    const plugins = [
+        [
+            "@babel/plugin-proposal-optional-chaining"
+        ]
+    ];
+  
+    return {
+      presets,
+      plugins
+    };
+  }
